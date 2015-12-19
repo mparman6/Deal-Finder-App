f51c478496af87e0ae30726fcdaef17a8985cb54
Johall <johnalexander.w.hall@gmail.com>
2015-12-19 13:02:47 -0500

    added ajax file for sqoot deals

--- js/deal.js
+++ js/deal.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
 
 
- $.getJSON("http://api.sqoot.com/v2/deals?api_key=demo&callback=?");
+ var sqootAPI = "http://api.sqoot.com/v2/deals?api_key=fflt53&callback=?";
+ var sqootKEY = "fflt53";
+
+
+ $.ajax ({
+  type:'GET',
+  url: sqootAPI,
+  data: 'format=json&id=123',
+  success:function(feed) {
+   console.log(feed);
+  },
+  dataType: 'jsonp'
+ });
+
+
+
+
+ // fflt53
 
 });
