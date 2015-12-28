48d0a1500793b014cbce7ff9c7cf687def305112
Johall <johnalexander.w.hall@gmail.com>
2015-12-27 21:55:33 -0500

    added ajax call for coupons / need to fix the parameters for location

--- js/deal.js
+++ js/deal.js
@@ -2,16 +2,28 @@
 
 $(document).ready(function() {
 
+
  $("button").click(function (e) {
 
   e.preventDefault();
 
+  var userLocation = $("#userLocation").val().trim();
 
-  $.getJSON("http://api.sqoot.com/v2/deals?api_key=fflt53&callback=?", function (data) {
-   console.log(data);
+
+
+  $.getJSON("http://api.sqoot.com/v2/deals?api_key=fflt53&callback=?", {
+    action: "query",
+    list: "search",
+    format: "json"
+  }, 
+
+  function (data) {
+
+    $("#dealsRow").empty();
+
    $.each(data.deals, function (i, item) {
-    $("<div>").attr("id", "colDealDiv").addClass("col-md-4")
-      .appendTo("#dealsRow");
+    $("<div>").attr("id", "colDealDiv").addClass("row col-md-8 col-md-offset-2")
+      .appendTo("body");
     $("<div>").attr("id", "thumbnailDealDiv").addClass("thumbnail")
       .appendTo("#colDealDiv");
     $("<div>").attr("id", "captionDiv").addClass("caption")
@@ -30,7 +42,50 @@ $(document).ready(function() {
       .appendTo("#thumbnailDealDiv");
     $("<img>").attr("src", data.deals[i].deal.image_url)
       .appendTo("#thumbnailDealDiv");
-   });                      
+
+   }); 
+
+
+
+  });   
+
+
+$.getJSON("http://api.sqoot.com/v2/coupons?api_key=fflt53&callback=?", {
+    action: "query",
+    list: "search",
+    format: "json"
+  }, 
+
+  function (data) {
+
+    $("#couponsRow").empty();
+
+   $.each(data.coupons, function (i, item) {
+    $("<div>").attr("id", "colCouponDiv").addClass("row col-md-8 col-md-offset-2")
+      .appendTo("body");
+    $("<div>").attr("id", "thumbnailCouponDiv").addClass("thumbnail")
+      .appendTo("#colCouponDiv");
+    $("<div>").attr("id", "captionDiv").addClass("caption")
+      .appendTo("#thumbnailCouponDiv");
+    $("<p>").append(data.coupons[i].coupon.category_name)
+      .appendTo("#thumbnailCouponDiv");
+    $("<p>").append(data.coupons[i].coupon.title)
+      .appendTo("#thumbnailCouponDiv");
+    $("<p>").append(data.coupons[i].coupon.merchants.name)
+      .appendTo("#thumbnailCouponDiv");
+    $("<p>").append(data.coupons[i].coupon.merchants.locality)
+      .append(" ")
+      .append(data.coupons[i].coupon.merchants.region)
+      .appendTo("#thumbnailCouponDiv");
+    $("<p>").append(data.query.location)
+      .appendTo("#thumbnailCouponDiv");
+    $("<img>").attr("src", data.coupons[i].coupon.image_url)
+      .appendTo("#thumbnailCouponDiv");
+
+   }); 
+
+
+
   }); 
 
  });
