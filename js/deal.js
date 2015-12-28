c5b1d1ced8ef2cfa991b64f6ebaf138b01dc4452
Johall <johnalexander.w.hall@gmail.com>
2015-12-27 22:22:00 -0500

    added variable names for parameters to clean up code

--- js/deal.js
+++ js/deal.js
@@ -21,26 +21,36 @@ $(document).ready(function() {
 
     $("#dealsRow").empty();
 
+
+
    $.each(data.deals, function (i, item) {
+
+    var categoryName = data.deals[i].deal.category_name;
+    var dealTitle = data.deals[i].deal.title;
+    var merchantName = data.deals[i].deal.merchant.name;
+    var merchantLocality = data.deals[i].deal.merchant.locality;
+    var merchantRegion = data.deals[i].deal.merchant.region;
+    var dealImage = data.deals[i].deal.image_url;
+
     $("<div>").attr("id", "colDealDiv").addClass("row col-md-8 col-md-offset-2")
       .appendTo("body");
     $("<div>").attr("id", "thumbnailDealDiv").addClass("thumbnail")
       .appendTo("#colDealDiv");
     $("<div>").attr("id", "captionDiv").addClass("caption")
       .appendTo("#thumbnailDealDiv");
-    $("<p>").append(data.deals[i].deal.category_name)
+    $("<p>").append(categoryName)
       .appendTo("#thumbnailDealDiv");
-    $("<p>").append(data.deals[i].deal.title)
+    $("<p>").append(dealTitle)
       .appendTo("#thumbnailDealDiv");
-    $("<p>").append(data.deals[i].deal.merchant.name)
+    $("<p>").append(merchantName)
       .appendTo("#thumbnailDealDiv");
-    $("<p>").append(data.deals[i].deal.merchant.locality)
+    $("<p>").append(merchantLocality)
       .append(" ")
-      .append(data.deals[i].deal.merchant.region)
+      .append(merchantRegion)
       .appendTo("#thumbnailDealDiv");
     $("<p>").append(data.query.location)
       .appendTo("#thumbnailDealDiv");
-    $("<img>").attr("src", data.deals[i].deal.image_url)
+    $("<img>").attr("src", dealImage)
       .appendTo("#thumbnailDealDiv");
 
    }); 
@@ -62,25 +72,34 @@ $.getJSON("http://api.sqoot.com/v2/coupons?api_key=fflt53&callback=?", {
     $("#couponsRow").empty();
 
    $.each(data.coupons, function (i, item) {
+
+    var categoryName = data.coupons[i].coupon.category_name;
+    var couponTitle = data.coupons[i].coupon.title;
+    var merchantName = data.coupons[i].coupon.merchants[0].merchant.name;
+    var merchantLocality = data.coupons[i].coupon.merchants[0].merchant.locality;
+    var merchantRegion = data.coupons[i].coupon.merchants[0].merchant.region;
+    var couponImage = data.coupons[i].coupon.image_url;
+
+    
     $("<div>").attr("id", "colCouponDiv").addClass("row col-md-8 col-md-offset-2")
       .appendTo("body");
     $("<div>").attr("id", "thumbnailCouponDiv").addClass("thumbnail")
       .appendTo("#colCouponDiv");
     $("<div>").attr("id", "captionDiv").addClass("caption")
       .appendTo("#thumbnailCouponDiv");
-    $("<p>").append(data.coupons[i].coupon.category_name)
+    $("<p>").append(categoryName)
       .appendTo("#thumbnailCouponDiv");
-    $("<p>").append(data.coupons[i].coupon.title)
+    $("<p>").append(couponTitle)
       .appendTo("#thumbnailCouponDiv");
-    $("<p>").append(data.coupons[i].coupon.merchants[0].merchant.name)
+    $("<p>").append(merchantName)
       .appendTo("#thumbnailCouponDiv");
-    $("<p>").append(data.coupons[i].coupon.merchants[0].merchant.locality)
+    $("<p>").append(merchantLocality)
       .append(" ")
-      .append(data.coupons[i].coupon.merchants[0].merchant.region)
+      .append(merchantRegion)
       .appendTo("#thumbnailCouponDiv");
     $("<p>").append(data.query.location)
       .appendTo("#thumbnailCouponDiv");
-    $("<img>").attr("src", data.coupons[i].coupon.image_url)
+    $("<img>").attr("src", couponImage)
       .appendTo("#thumbnailCouponDiv");
 
    }); 
