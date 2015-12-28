fe5975b1c59be2851aa1fac309784446a1d6a3f7
Johall <johnalexander.w.hall@gmail.com>
2015-12-28 18:48:52 -0500

    experimented with iteration

--- js/deal.js
+++ js/deal.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
    $.each(data.deals, function buildDealThumbnail (i, item) {
 
     var api_key = "fflt53";
+    var dealId = data.deals[i].deal.id;
     var categoryName = $("<p>").append(data.deals[i].deal.category_name);
     var dealTitle = $("<p>").append(data.deals[i].deal.title);
     var merchantName = $("<p>").append(data.deals[i].deal.merchant.name);
@@ -73,6 +74,7 @@ $.getJSON("http://api.sqoot.com/v2/coupons?api_key=fflt53&callback=?", {
    $.each(data.coupons, function buildCouponThumbnail (i, item) {
 
     var api_key = "fflt53";
+    var couponId = data.coupons[i].coupon.id;
     var categoryName = data.coupons[i].coupon.category_name;
     var couponTitle = data.coupons[i].coupon.title;
     var merchantName = data.coupons[i].coupon.merchants[0].merchant.name;
@@ -80,11 +82,15 @@ $.getJSON("http://api.sqoot.com/v2/coupons?api_key=fflt53&callback=?", {
     var merchantRegion = data.coupons[i].coupon.merchants[0].merchant.region;
     var couponImage = data.coupons[i].coupon.image_url;
 
+    var colCouponDiv = $("<div>").attr("id", "colCouponDiv").addClass("row col-md-8 col-md-offset-2");
+    var thumbnailCouponDiv = $("<div>").addClass("thumbnail");
+    var captionDiv = $("<div>").addClass("caption");
+
     $("<div>").attr("id", "colCouponDiv").addClass("row col-md-8 col-md-offset-2")
       .appendTo("body");
     $("<div>").attr("id", "thumbnailCouponDiv").addClass("thumbnail")
       .appendTo("#colCouponDiv");
-    $("<div>").attr("id", "captionDiv").addClass("caption")
+    $("<div>").addClass("caption")
       .appendTo("#thumbnailCouponDiv");
     $("<p>").append(categoryName)
       .appendTo("#thumbnailCouponDiv");
