6efd314cd7690a1902274e74f59f815c2c1d3a4a
Johall <johnalexander.w.hall@gmail.com>
2015-12-29 20:50:04 -0500

    added personal js folder to master

--- js/deal.js
+++ js/deal.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
 
 
@@ -7,105 +9,104 @@ $(document).ready(function() {
 
   var userLocation = $("#userLocation").val().trim();
 
-  $.getJSON("http://api.sqoot.com/v2/deals?api_key=fflt53&callback=?", {
+  var sqootDealAPI = "http://api.sqoot.com/v2/deals?api_key=fflt53&callback=?";
+  sqootDealAPI += userLocation;
+
+  $.getJSON(sqootDealAPI, {
     action: "query",
     list: "search",
     format: "json"
   }, 
 
   function (data) {
-
     console.log(data);
     $("#dealsRow").empty();
 
-   $.each(data.deals, function buildDealThumbnail (i, item) {
+    $.each(data.deals, function buildDealThumbnail (i, item) {
+
+      var api_key = "fflt53";
+      var dealId = data.deals[i].deal.id;
+      var categoryName = $("<p>").append(data.deals[i].deal.category_name);
+      var dealTitle = $("<p>").append(data.deals[i].deal.title);
+      var merchantName = $("<p>").append(data.deals[i].deal.merchant.name);
+      var merchantLocality = $("<p>").append(data.deals[i].deal.merchant.locality);
+      var merchantRegion = $("<p>").append(data.deals[i].deal.merchant.region);
+      var dealImage =$("<img>").attr("src", data.deals[i].deal.image_url).addClass("thumbnailImage");
+
+      var colDealDiv = $("<div>").attr("id", "colDealDiv").addClass("row col-md-8 col-md-offset-2");
+      var thumbnailDealDiv = $("<div>").attr("id", "thumbnailDealDiv").addClass("thumbnail");
+      var captionDiv = $("<div>").attr("id", "captionDealDiv").addClass("caption");
+
+
+
+      $("#dealsRow").append(colDealDiv
+        .append(thumbnailDealDiv
+          .append(captionDiv
+            .append(categoryName)
+            .append(dealTitle)
+            .append(merchantName)
+            .append(merchantLocality)
+            .append(merchantRegion)
+            .append(dealImage)
+            )
+          )
+        );
+    });
+
+});   
+
+  var userLocation = $("#userLocation").val().trim();
+
+  var sqootCouponAPI = "http://api.sqoot.com/v2/coupons?api_key=fflt53&callback=?";
+  sqootCouponAPI += userLocation;
+
+
+$.getJSON(sqootCouponAPI, {
+  action: "query",
+  list: "search",
+  format: "json"
+}, 
+
+function (data) {
+  console.log(data);
+
+  $("#couponsRow").empty();
+
+  $.each(data.coupons, function buildCouponThumbnail (i, item) {
 
     var api_key = "fflt53";
-    var categoryName = $("<p>").append(data.deals[i].deal.category_name);
-    var dealTitle = $("<p>").append(data.deals[i].deal.title);
-    var merchantName = $("<p>").append(data.deals[i].deal.merchant.name);
-    var merchantLocality = $("<p>").append(data.deals[i].deal.merchant.locality);
-    var merchantRegion = $("<p>").append(data.deals[i].deal.merchant.region);
-    var dealImageParam = data.deals[i].deal.image_url;
+    var couponId = data.coupons[i].coupon.id;
+    var categoryName = $("<p>").append(data.coupons[i].coupon.category_name);
+    var couponTitle = $("<p>").append(data.coupons[i].coupon.title);
+    var merchantName = $("<p>").append(data.coupons[i].coupon.merchants[0].merchant.name);
+    var merchantLocality = $("<p>").append(data.coupons[i].coupon.merchants[0].merchant.locality);
+    var merchantRegion = $("<p>").append(data.coupons[i].coupon.merchants[0].merchant.region);
+    var couponImage = $("<img>").attr("src", data.coupons[i].coupon.image_url).addClass("thumbnailImage");
 
-    var colDealDiv = $("<div>").attr("id", "colDealDiv").addClass("row col-md-8 col-md-offset-2");
-    var thumbnailDealDiv = $("<div>").addClass("thumbnail");
-    var dealImage = $("<img>").attr("src", dealImage);
-    var captionDiv = $("<div>").addClass("caption");
-    var captionDiv = $("<div>").addClass("caption");
+    var colCouponDiv = $("<div>").attr("id", "colCouponDiv").addClass("row col-md-8 col-md-offset-2");
+    var thumbnailCouponDiv = $("<div>").attr("id", "thumbnailCouponDiv").addClass("thumbnail");
+    var captionDiv = $("<div>").attr("id", "captionCouponDiv").addClass("caption");
 
-
-
-    $("<div>").attr("id", "colDealDiv").addClass("row col-md-8 col-md-offset-2")
-      .appendTo("body");
-    $("<div>").attr("id", "thumbnailDealDiv").addClass("thumbnail")
-      .appendTo("#colDealDiv");
-    $("<div>").attr("id", "captionDiv").addClass("caption")
-      .appendTo("#thumbnailDealDiv");
-    $("<p>").append(categoryName)
-      .appendTo("#thumbnailDealDiv");
-    $("<p>").append(dealTitle)
-      .appendTo("#thumbnailDealDiv");
-    $("<p>").append(merchantName)
-      .appendTo("#thumbnailDealDiv");
-    $("<p>").append(merchantLocality)
-      .append(" ")
-      .append(merchantRegion)
-      .appendTo("#thumbnailDealDiv");
-    $("<img>").attr("src", dealImageParam)
-      .appendTo("#thumbnailDealDiv");
-   });
-
-  });   
-
-
-$.getJSON("http://api.sqoot.com/v2/coupons?api_key=fflt53&callback=?", {
-    action: "query",
-    list: "search",
-    format: "json"
-  }, 
-
-  function (data) {
-    console.log(data);
-
-    $("#couponsRow").empty();
-
-   $.each(data.coupons, function buildCouponThumbnail (i, item) {
-
-    var api_key = "fflt53";
-    var categoryName = data.coupons[i].coupon.category_name;
-    var couponTitle = data.coupons[i].coupon.title;
-    var merchantName = data.coupons[i].coupon.merchants[0].merchant.name;
-    var merchantLocality = data.coupons[i].coupon.merchants[0].merchant.locality;
-    var merchantRegion = data.coupons[i].coupon.merchants[0].merchant.region;
-    var couponImage = data.coupons[i].coupon.image_url;
-
-    $("<div>").attr("id", "colCouponDiv").addClass("row col-md-8 col-md-offset-2")
-      .appendTo("body");
-    $("<div>").attr("id", "thumbnailCouponDiv").addClass("thumbnail")
-      .appendTo("#colCouponDiv");
-    $("<div>").attr("id", "captionDiv").addClass("caption")
-      .appendTo("#thumbnailCouponDiv");
-    $("<p>").append(categoryName)
-      .appendTo("#thumbnailCouponDiv");
-    $("<p>").append(couponTitle)
-      .appendTo("#thumbnailCouponDiv");
-    $("<p>").append(merchantName)
-      .appendTo("#thumbnailCouponDiv");
-    $("<p>").append(merchantLocality)
-      .append(" ")
-      .append(merchantRegion)
-      .appendTo("#thumbnailCouponDiv");
-    $("<img>").attr("src", couponImage)
-      .appendTo("#thumbnailCouponDiv");
-
-
-   }); 
-
+    $("#couponsRow").append(colCouponDiv
+      .append(thumbnailCouponDiv
+        .append(captionDiv
+          .append(categoryName)
+          .append(couponTitle)
+          .append(merchantName)
+          .append(merchantLocality)
+          .append(merchantRegion)
+          .append(couponImage)
+          )
+        )
+      );
   }); 
 
- }); //end button click
+}); 
 
+ }); //end button click
 }); //ends document ready
+
+
+
 
 
