ad93c4050d4fbe835407fc6fee729faf683e7913
MD4472 <dnlp200@gmail.com>
2016-01-02 18:25:58 -0500

    Added map into html file

--- js/deal.js
+++ js/deal.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-<<<<<<< HEAD
+
 
 // $('#blue').on("click", function(){
 //   $('#modal').modal('show');
@@ -19,8 +19,7 @@ $(document).ready(function() {
   navigator.geolocation.getCurrentPosition(curr_pos);
   return false;
 });
-=======
->>>>>>> 8f35fe52a9de33e64316392bd984fe3972820f99
+
 
 
  $("button").click(function (e) {
@@ -37,10 +36,9 @@ $(document).ready(function() {
 
   function (data) {
 
-<<<<<<< HEAD
-=======
+
     console.log(data);
->>>>>>> 8f35fe52a9de33e64316392bd984fe3972820f99
+
     $("#dealsRow").empty();
 
    $.each(data.deals, function buildDealThumbnail (i, item) {
@@ -56,10 +54,10 @@ $(document).ready(function() {
     var colDealDiv = $("<div>").attr("id", "colDealDiv").addClass("row col-md-8 col-md-offset-2");
     var thumbnailDealDiv = $("<div>").addClass("thumbnail");
     var dealImage = $("<img>").attr("src", dealImage);
-<<<<<<< HEAD
+
     var captionDiv = $("<div>").addClass("caption");
 
-=======
+
     var captionDiv = $("<div>").addClass("caption");
     var captionDiv = $("<div>").addClass("caption");
 
@@ -83,7 +81,7 @@ $(document).ready(function() {
       .appendTo("#thumbnailDealDiv");
     $("<img>").attr("src", dealImageParam)
       .appendTo("#thumbnailDealDiv");
->>>>>>> 8f35fe52a9de33e64316392bd984fe3972820f99
+
    });
 
   });   
