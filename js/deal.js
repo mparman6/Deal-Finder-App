12cef2bc1bef673261cfdcc002e595e758e39942
Johall <johnalexander.w.hall@gmail.com>
2016-01-04 01:11:59 -0500

    no luck with traversing dom for panel footers

--- js/deal.js
+++ js/deal.js
@@ -26,6 +26,10 @@ $("#userLocation").keyup(function(event){
   }
 });
 
+$(document).on("click", ".panel .btn-info", function() {
+  $(this).closest('.panel-body').next(".panel-footer").collapse("toggle");
+});
+
 
 $("#locationBtn").click(function (e) {
 
@@ -47,12 +51,10 @@ $("#locationBtn").click(function (e) {
 
   function (data) {
 
-
     console.log(data);
 
     $.each(data.deals, function buildDealThumbnail (i, item) {
 
-      var api_key = "fflt53";
       var dealId = data.deals[i].deal.id;
       var categoryName = $("<p>").append(data.deals[i].deal.category_name);
       var dealTitle = $("<p>").append(data.deals[i].deal.title);
@@ -61,43 +63,49 @@ $("#locationBtn").click(function (e) {
       var merchantRegion = $("<p>").append(data.deals[i].deal.merchant.region);
       var dealImage =$("<img>").attr("src", data.deals[i].deal.image_url).addClass("img-thumbnail");
 
-      var colDealDiv = $("<div>").attr("id", "colDealDiv").addClass("row col-md-8 col-md-offset-2");
+      var dealExpire = $("<p>").append(data.deals[i].deal.expires_at);
+      var dealPrice = $("<p>").append(data.deals[i].deal.price);
+      var dealDescription = $("<p>").append(data.deals[i].deal.short_title);
+      var dealUrl = $("<p>").append(data.deals[i].deal.url);
+
+      var colDiv = $("<div>").attr("id", "colDealDiv").addClass("row col-md-8 col-md-offset-2 colDiv");
+      var dealPanel = $("<div>").addClass("panel panel-default");
+      var dealPanelHead = $("<div>").addClass("panel-heading");
+      var dealPanelBody = $("<div>").addClass("panel-body");
+      var dealButtonInfo = $("<button>").addClass("btn btn-info").attr("data-toggle", "collapse").html("More Info");
+      var dealPanelFooter = $("<div>").addClass("panel-footer collapse");
       var thumbnailDealDiv = $("<div>").attr("id", "thumbnailDealDiv").addClass("thumbnail");
       var captionDiv = $("<div>").attr("id", "captionDealDiv").addClass("caption");
 
-      var dealIdUrl = "http://api.sqoot.com/v2/deals/"
-      dealIdUrl += dealId[0];
-      dealIdUrl += "?api_key=";
-      dealIdUrl += api_key;
 
+      $("#dealsRow").append(colDiv
+        .append (dealPanel
+          .append(dealPanelHead
+            .append(categoryName)
+            )
+          .append(dealPanelBody
+            .append(thumbnailDealDiv
+              .append(captionDiv
 
-$(".thumbnail").click(function (data) {
-$.getJSON(dealIdUrl).success(function(data) {
-   $('#myModal, div').replaceWith(data.deals);
-   $('#myModal').modal("show");
-   return false;
-});
-});
+                .append(dealTitle)
+                .append(merchantName)
+                .append(merchantLocality)
+                .append(merchantRegion)
+                .append(dealImage)
+                .append(dealButtonInfo)
+                )
+              )
+            )
+          .append(dealPanelFooter
+            .append(dealDescription)
+            .append(dealPrice)
+            .append(dealExpire)
+            .append(dealUrl)
+            )
+          )
+        );
 
-
-
-$("#dealsRow").append(colDealDiv
-  .append(thumbnailDealDiv
-    .append(captionDiv
-      .append(categoryName)
-      .append(dealTitle)
-      .append(merchantName)
-      .append(merchantLocality)
-      .append(merchantRegion)
-      .append(dealImage)
-      )
-    )
-  );
-
-
-
-});
-
+    });
 });   
 
 var userLocation = $("#userLocation").val().trim();
@@ -117,7 +125,6 @@ function (data) {
 
   $.each(data.coupons, function buildCouponThumbnail (i, item) {
 
-    var api_key = "fflt53";
     var couponId = data.coupons[i].coupon.id;
     var categoryName = $("<p>").append(data.coupons[i].coupon.category_name);
     var couponTitle = $("<p>").append(data.coupons[i].coupon.title);
@@ -126,30 +133,56 @@ function (data) {
     var merchantRegion = $("<p>").append(data.coupons[i].coupon.merchants[0].merchant.region);
     var couponImage = $("<img>").attr("src", data.coupons[i].coupon.image_url).addClass("img-thumbnail");
 
-    var colCouponDiv = $("<div>").attr("id", "colCouponDiv").addClass("row col-md-8 col-md-offset-2");
+    var couponExpire = $("<p>").append(data.coupons[i].coupon.expires_at);
+    var couponPrice = $("<p>").append(data.coupons[i].coupon.title);
+    var couponDescription = $("<p>").append(data.coupons[i].coupon.description);
+    var couponUrl = $("<p>").append(data.coupons[i].coupon.url);
+
+    var colDiv = $("<div>").attr("id", "colCouponDiv").addClass("row col-md-8 col-md-offset-2 colDiv");
     var thumbnailCouponDiv = $("<div>").attr("id", "thumbnailCouponDiv").addClass("thumbnail");
     var captionDiv = $("<div>").attr("id", "captionCouponDiv").addClass("caption");
+    var couponPanel = $("<div>").addClass("panel panel-default");
+    var couponPanelHead = $("<div>").addClass("panel-heading");
+    var couponPanelBody = $("<div>").addClass("panel-body");
+    var couponButtonInfo = $("<button>").addClass("btn btn-info infoBtn").attr("data-toggle", "collapse").html("More Info");
+    var couponPanelFooter = $("<div>").addClass("panel-footer collapse");
 
 
 
 
-    $("#couponsRow").append(colCouponDiv
-      .append(thumbnailCouponDiv
-        .append(captionDiv
+    $("#couponsRow").append(colDiv
+      .append (couponPanel
+        .append(couponPanelHead
           .append(categoryName)
-          .append(couponTitle)
-          .append(merchantName)
-          .append(merchantLocality)
-          .append(merchantRegion)
-          .append(couponImage)
+          )
+        .append(couponPanelBody
+          .append(thumbnailCouponDiv
+            .append(captionDiv
+
+              .append(couponTitle)
+              .append(merchantName)
+              .append(merchantLocality)
+              .append(merchantRegion)
+              .append(couponImage)
+              .append(couponButtonInfo)
+              )
+            )
+          )
+        .append(couponPanelFooter
+          .append(couponDescription)
+          .append(couponPrice)
+          .append(couponExpire)
+          .append(couponUrl)
           )
         )
       );
-
-
   }); 
 
 }); 
+
+
+
+
 
  }); //end button click
 
