80735a4c3b9f729ffc08102b37d27421b79c52f3
Matt <mparman6@yahoo.com>
2016-01-04 14:51:11 -0500

    created a redirect to make page mobile

--- js/redirect.js
+++ js/redirect.js
@@ -1,9 +1,10 @@
 $(function() {
- var ipad = true;               // Set to false to not redirect on iPad.
-var other_tablets = true;  // Set to false to not redirect on other tablets.
-var mobile_domain = 'immense-forest-3648.bmobilized.com';
-var path = encodeURIComponent(location.href);
-var page = "?&url=" + path;
-document.write('<link rel="alternate" media="only screen and (max-device-width: 1024px)" href="' + location.protocol + '//' + mobile_domain + page + '" >');
-document.write(unescape("%3Cscript src='" + location.protocol + "//s3.amazonaws.com/me.static/js/me.redirect.min.js' type='text/javascript'%3E%3C/script%3E"));
+
+  var mobile_domain ="immense-forest-3648.bmobilized.com";
+// Set to false to not redirect on iPad.
+var ipad = true;
+// Set to false to not redirect on other tablets (Android , BlackBerry, WebOS tablets).
+var other_tablets = true;
+document.write(unescape("%3Cscript src='"+location.protocol+"//s3.amazonaws.com/me.static/js/me.redirect.min.js' type='text/javascript'%3E%3C/script%3E"));
+
 });
